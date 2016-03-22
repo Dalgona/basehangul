d9e39052fc822959008dd7db5fe0d7347c29da80
Dalgona <dalgona@hontou.moe>
2016-03-22 19:53:44 +0900

    Release 0.1.0
    Minor code cleanup
    Add and modify LICENSE and README.md

--- README.md
+++ README.md
@@ -12,3 +12,12 @@ You can do whatever you wanna do as long as you **do not** sell the source code 
 
 This software is provided **"AS IS"**. I **do not** garuntee that this software will work correctly forever and I **am not** responsible for any loss of data caused by this software.
 
+## Changelog
+
+### 0.1.0 (22 March 2016)
+
+Initial development release. `basehangul` is now available on [Hex](https://hex.pm/packages/basehangul/0.1.0).
+
+### `nil` (22 March 2016)
+
+Initial commit.
--- mix.exs
+++ mix.exs
@@ -3,30 +3,26 @@ defmodule BaseHangul.Mixfile do
 
   def project do
     [app: :basehangul,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.2",
+     description: "Elixir implementation of BaseHangul.",
+     package: package,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :iconv]]
+    [applications: [:iconv]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [{:iconv, "~> 1.0"}]
+  end
+
+  defp package do
+    [maintainers: ["Dalgona."],
+     links: %{GitHub: "https://github.com/Dalgona/basehangul"},
+     licenses: ["See GitHub page"]]
   end
 end
