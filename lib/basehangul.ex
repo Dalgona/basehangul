0069671063ccd77d8835c1764e16d4ddda23ed68
Dalgona <dalgona@hontou.moe>
2016-03-23 18:39:31 +0900

    +documentation

--- lib/basehangul.ex
+++ lib/basehangul.ex
@@ -1,21 +1,95 @@
 defmodule BaseHangul do
+  @moduledoc ~S"""
+  This module provides BaseHangul encoding/decoding functionalities.
+
+  Visit the [GitHub Page](https://github.com/dalgona/basehangul) for more information.
+  """
+
   use Bitwise
 
   @padchr [0xc8, 0xe5]
 
+  @doc ~S"""
+  Encodes `input` to a BaseHangul string. `input` can be a string or a binary.
+
+  Returns a BaseHangul-encoded string on success.
+
+  ## Examples
+
+      iex> BaseHangul.encode("Hello")
+      "낏뗐맸굉"
+      iex> BaseHangul.encode(<<1, 2, 3, 4, 5>>)
+      "갈걍꺅갉"
+
+  """
   def encode(input) when is_binary(input) do
     {:ok, sio} = StringIO.open input
     Stream.map(IO.binstream(sio, 5), &(encunit &1)) |> Enum.join("")
   end
 
+  @doc ~S"""
+  Decodes BaseHangul-encoded string `input`.
+
+  Returns the decoded result as a string or a binary.
+
+  ## Examples
+
+      iex> BaseHangul.decode("낏뗐맸굉")
+      "Hello"
+      iex> BaseHangul.decode("갈걍꺅갉")
+      <<1, 2, 3, 4, 5>>
+
+  ## Notes
+
+  This function expects a UTF-8 encoded text.
+  Passing an invalid BaseHangul-encoded string as an input will result in an
+  undefined behavior.
+
+  """
   def decode(input) when is_binary(input) do
     {:ok, sio} = StringIO.open input
     Stream.map(IO.binstream(sio, 12), &(decunit &1)) |> Enum.join("")
   end
 
+  @doc ~S"""
+  Takes data from `indev` IO device and writes BaseHangul-encoded string
+  to `outdev` IO device.
+
+  Returns `:ok` on success.
+
+  ## Examples
+
+      iex> {:ok, file} = File.open("some_file", [:read])
+      {:ok, #PID<0.XXX.0>}
+      iex> BaseHangul.encode(file, :stdio)
+      넥라넒굔덥램꼬먈눠멂불뮐궂뚠붉걍닐묩묻굡궂릅는놨궂몹듕낍
+      # ... more ...
+      놨궂몹듕낍닥듈럇낍뉴름댜둥:ok
+
+  """
   def encode(indev, outdev \\ Process.group_leader()),
     do: Enum.each IO.binstream(indev, 5), &(IO.write(outdev, encunit &1))
 
+  @doc ~S"""
+  Takes BaseHangul-encoded data from `indev` IO device and writes decoded data
+  to `outdev` IO device.
+
+  Returns `:ok` on success.
+
+  ## Examples
+
+      iex> {:ok, file} = File.open("some_file", [:read])
+      {:ok, #PID<0.XXX.0>}
+      iex> BaseHangul.decode(file, :stdio)
+      This is an encoded string.:ok
+
+  ## Notes
+
+  This function expects a UTF-8 encoded text.
+  Passing an invalid BaseHangul-encoded string as an input will result in an
+  undefined behavior.
+
+  """
   def decode(indev, outdev \\ Process.group_leader()),
     do: Enum.each IO.binstream(indev, 12), &(IO.write(outdev, decunit &1))
 
