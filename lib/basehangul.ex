81df986942da03e4d8ac869ae61f20397a52e0ce
Dalgona <dalgona@hontou.moe>
2019-02-07 21:10:39 +0900

    Add .formatter.exs and run the code formatter

--- lib/basehangul.ex
+++ lib/basehangul.ex
@@ -2,12 +2,13 @@ defmodule BaseHangul do
   @moduledoc ~S"""
   This module provides BaseHangul encoding/decoding functionalities.
 
-  Visit the [GitHub Page](https://github.com/dalgona/basehangul) for more information.
+  Visit the [GitHub Page](https://github.com/dalgona/basehangul)
+  for more information.
   """
 
   use Bitwise
 
-  @padchr [0xc8, 0xe5]
+  @padchr [0xC8, 0xE5]
 
   @doc ~S"""
   Encodes `input` to a BaseHangul string. `input` can be a string or a binary.
@@ -22,9 +23,10 @@ defmodule BaseHangul do
       "갈걍꺅갉"
 
   """
+  @spec encode(binary()) :: binary()
   def encode(input) when is_binary(input) do
-    {:ok, sio} = StringIO.open input
-    Stream.map(IO.binstream(sio, 5), &(encunit &1)) |> Enum.join("")
+    {:ok, sio} = StringIO.open(input)
+    Stream.map(IO.binstream(sio, 5), &encunit(&1)) |> Enum.join("")
   end
 
   @doc ~S"""
@@ -47,8 +49,8 @@ defmodule BaseHangul do
 
   """
   def decode(input) when is_binary(input) do
-    {:ok, sio} = StringIO.open input
-    Stream.map(IO.binstream(sio, 12), &(decunit &1)) |> Enum.join("")
+    {:ok, sio} = StringIO.open(input)
+    Stream.map(IO.binstream(sio, 12), &decunit(&1)) |> Enum.join("")
   end
 
   @doc ~S"""
@@ -68,7 +70,7 @@ defmodule BaseHangul do
 
   """
   def encode(indev, outdev \\ Process.group_leader()),
-    do: Enum.each IO.binstream(indev, 5), &(IO.write(outdev, encunit &1))
+    do: Enum.each(IO.binstream(indev, 5), &IO.write(outdev, encunit(&1)))
 
   @doc ~S"""
   Takes BaseHangul-encoded data from `indev` IO device and writes decoded data
@@ -91,55 +93,82 @@ defmodule BaseHangul do
 
   """
   def decode(indev, outdev \\ Process.group_leader()),
-    do: Enum.each IO.binstream(indev, 12), &(IO.write(outdev, decunit &1))
+    do: Enum.each(IO.binstream(indev, 12), &IO.write(outdev, decunit(&1)))
 
   defp encunit(x), do: :iconv.convert("euc-kr", "utf-8", x |> repack_8to10 |> to_euclist([]))
   defp decunit(x), do: :iconv.convert("utf-8", "euc-kr", x) |> to_ordlist([]) |> repack_10to8
 
   defp repack_8to10(bin) when byte_size(bin) <= 5 do
-    sz = byte_size bin
+    sz = byte_size(bin)
     zpad = 8 * (5 - sz)
     <<bignum::40>> = bin <> <<0::size(zpad)>>
-    {(bignum |> repack_8to10_rev([]) |> Enum.reverse), sz}
+    {bignum |> repack_8to10_rev([]) |> Enum.reverse(), sz}
   end
 
   defp to_euclist({list, sz}, out) when length(list) > 0 do
-    [h|t] = list
+    [h | t] = list
+
     case length(list) do
-      4 -> to_euclist({t, sz}, out ++ get_euc(h))
+      4 ->
+        to_euclist({t, sz}, out ++ get_euc(h))
+
       1 ->
-        out ++ (if h == 0 and sz < 4 do @padchr
-        else (if sz == 4, do: get_euc(bor(h >>> 8, 1024)), else: get_euc(h)) end)
+        out ++
+          if h == 0 and sz < 4 do
+            @padchr
+          else
+            if sz == 4, do: get_euc(bor(h >>> 8, 1024)), else: get_euc(h)
+          end
+
       _ ->
-        to_euclist({t, sz}, out ++ (
-          if h == 0 and sz <= (4 - length(list)) do @padchr else get_euc(h) end
-        ))
+        to_euclist(
+          {t, sz},
+          out ++
+            if h == 0 and sz <= 4 - length(list) do
+              @padchr
+            else
+              get_euc(h)
+            end
+        )
     end
   end
 
   defp to_ordlist(<<>>, out), do: out
+
   defp to_ordlist(eucstr, out) do
     try do
       <<n1, n2>> <> rest = eucstr
-      to_ordlist(rest, out ++ (if [n1, n2] == @padchr do [false] else [get_ord(n1, n2)] end))
+
+      to_ordlist(
+        rest,
+        out ++
+          if [n1, n2] == @padchr do
+            [false]
+          else
+            [get_ord(n1, n2)]
+          end
+      )
     rescue
       MatchError -> raise ArgumentError, message: "Not a vaild BaseHangul string"
     end
   end
 
   defp repack_10to8(ordlist) do
-    tmp = Enum.map ordlist, &(if &1 == false, do: 0, else: &1)
-    sz = Enum.find_index ordlist, &(&1==false)
-    last = List.last tmp
+    tmp = Enum.map(ordlist, &if(&1 == false, do: 0, else: &1))
+    sz = Enum.find_index(ordlist, &(&1 == false))
+    last = List.last(tmp)
+
     case sz do
       x when is_number(x) or (is_nil(x) and last < 1024) ->
         sz = sz || 5
+
       _ ->
         sz = 4
-        tmp = List.update_at tmp, 3, &((&1 - 1024) <<< 8)
+        tmp = List.update_at(tmp, 3, &((&1 - 1024) <<< 8))
     end
-    bigint = Enum.reduce(tmp, 0, fn(x, a) -> (a <<< 10) + x end)
-    binary_part <<bigint::40>>, 0, sz
+
+    bigint = Enum.reduce(tmp, 0, fn x, a -> (a <<< 10) + x end)
+    binary_part(<<bigint::40>>, 0, sz)
   end
 
   defp repack_8to10_rev(0, list) when length(list) == 4, do: list
@@ -148,12 +177,13 @@ defmodule BaseHangul do
   defp get_euc(ord) do
     cond do
       ord > 1027 -> raise ArgumentError, message: "Character ordinal out of range"
-      true -> [0xb0 + div(ord, 94), 0xa1 + rem(ord, 94)]
+      true -> [0xB0 + div(ord, 94), 0xA1 + rem(ord, 94)]
     end
   end
 
   defp get_ord(n1, n2) do
-    num = (n1 - 0xb0) * 94 + (n2 - 0xa1)
+    num = (n1 - 0xB0) * 94 + (n2 - 0xA1)
+
     cond do
       num > 1027 or num < 0 -> raise ArgumentError, message: "Not a valid BaseHangul string"
       true -> num
--- mix.exs
+++ mix.exs
@@ -2,14 +2,16 @@ defmodule BaseHangul.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :basehangul,
-     version: "0.2.1",
-     elixir: "~> 1.6",
-     description: "Elixir implementation of BaseHangul.",
-     package: package,
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :basehangul,
+      version: "0.2.1",
+      elixir: "~> 1.6",
+      description: "Elixir implementation of BaseHangul.",
+      package: package(),
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
   end
 
   def application do
@@ -17,13 +19,14 @@ defmodule BaseHangul.Mixfile do
   end
 
   defp deps do
-    [{:iconv, "~> 1.0"},
-     {:ex_doc, "0.18.4", only: [:dev], runtime: false}]
+    [{:iconv, "~> 1.0"}, {:ex_doc, "0.18.4", only: [:dev], runtime: false}]
   end
 
   defp package do
-    [maintainers: ["Dalgona."],
-     links: %{GitHub: "https://github.com/Dalgona/basehangul"},
-     licenses: ["See GitHub page"]]
+    [
+      maintainers: ["Dalgona."],
+      links: %{GitHub: "https://github.com/Dalgona/basehangul"},
+      licenses: ["See GitHub page"]
+    ]
   end
 end
