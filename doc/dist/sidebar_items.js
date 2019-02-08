5cd0160d3a698f649f07f183971a5f6abacc7ce3
Dalgona <dalgona@hontou.moe>
2019-02-08 13:53:34 +0900

    Cleanup repo

--- doc/dist/sidebar_items.js
+++
@@ -1,1 +0,0 @@
-sidebarNodes={"exceptions":[],"extras":[{"id":"api-reference","title":"API Reference","headers":[]}],"modules":[{"id":"BaseHangul","title":"BaseHangul","functions":[{"id":"decode/1","anchor":"decode/1"},{"id":"decode/2","anchor":"decode/2"},{"id":"encode/1","anchor":"encode/1"},{"id":"encode/2","anchor":"encode/2"}]}],"protocols":[]}
